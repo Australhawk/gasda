6745feb3b37cfbdd3368baa0f59760ce63a25d89
Sebastian Paiva <austral@gamerpvp.com>
2014-03-25 20:44:05 -0300

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GamerPVP::Application.config.secret_key_base = '1838bcc0806fe773e91162f853d6a59e2c6ec22380077fa637e1f8339f2f676779235808875e52821e3fb97fe0ee9ae6cc7c1ad1ca23c41434c5d3c4f56e7cd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GamerPVP::Application.config.session_store :cookie_store, key: '_GamerPVP_session'
