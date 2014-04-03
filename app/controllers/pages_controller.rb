6ea3a55ebedb7cc9c9a1f42f90367e0e605eea15
SebaVargas <vargas.sebastian07@gmail.com>
2014-04-02 21:24:44 -0300

    último commit miércoles 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
 	#Todas la paginas que no necesitan que el usuario este registrado
+	before_filter :hide_header
+	before_filter :hide_footer, only: [:landing]
   def index
   	@user = User.new
   	if current_user
   		redirect_to main_path
   	end
+  end
+  protected
+  def hide_header
+    @hide_header = true
+  end
+  def hide_footer
+    @hide_footer = true
   end
 end
