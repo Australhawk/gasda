873f089636f99b300196848503f97553ccc4d2b8
Sebastian Paiva <austral@gamerpvp.com>
2014-04-10 16:50:57 -0300

    Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	def create
 		user = User.create(user_parameters)
 		if user.save
-			redirect_to main_path, :alert => 'Registered'
+			redirect_to home_path, :alert => 'Registered'
 		else
 			@user = user
 			flash.now.alert = "Fill all pls"
