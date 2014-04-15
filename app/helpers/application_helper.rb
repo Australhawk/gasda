5b071f7b94b1892636c0998502c9f5cee86cacdd
Sebastian Paiva <austral@gamerpvp.com>
2014-04-15 06:31:36 -0300

    Removidas cosas innecesarias

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,16 +3,6 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
-  def get_username(user)
-    if user.username.nil?
-      user.email
-    else
-      user.username.capitalize
-    end
-  end
-  def get_team_role(user,team)
-    user.team_members.where(team_id: team.id).take.role.name
-  end
   def cp(path)
   "current" if current_page?(path)
   end
